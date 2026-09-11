7e55a48c67b6fa9567d46505a07579d5f4313838
ilcubo <sasp3name16f@gmail.com>
2026-09-11 07:47:09 +0700

    ✨ Complete texture.c

--- texture.c
+++ texture.c
@@ -9,9 +9,15 @@ void loadTexture(GLuint *texture_id, char *texture_path) {
     unsigned char *data = stbi_load(texture_path, &width, &height, &nr_channels, 3);
     glGenTextures(1, texture_id);
     glBindTexture(GL_TEXTURE_2D, *texture_id);
-    if (data != NULL) {
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    if (data == NULL) {
         fprintf(stderr, "ERROR: Failed to load texture");
+    } else {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
     }
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
+    stbi_image_free(data);
 }
